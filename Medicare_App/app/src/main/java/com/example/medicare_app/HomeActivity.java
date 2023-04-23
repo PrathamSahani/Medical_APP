4631f34866af56b8130e9ed34b77b5505803f37f
PRATHAM SAHANI <pratham.sahani2020@vitbhopal.ac.in>
2023-04-24 01:57:05 +0530

    APP

---
+++ Medicare_App/app/src/main/java/com/example/medicare_app/HomeActivity.java
@@ -0,0 +1,54 @@
+package com.example.medicare_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedpreferences.getString("usrname", "").toString();
+        Toast.makeText(getApplicationContext(), "Welcome"+username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardExitCard);
+        exit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+
+            }
+            });
+        CardView findDoctor = findViewById(R.id.cardFindDoctor);
+        findDoctor.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(HomeActivity.this , FindDoctorActivity.class));
+            }
+
+        });
+        CardView labtest = findViewById(R.id.cardLabTest);
+        labtest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(HomeActivity.this , LabTextActivity.class));
+            }
+
+        });
+
+
+        }
+    }
